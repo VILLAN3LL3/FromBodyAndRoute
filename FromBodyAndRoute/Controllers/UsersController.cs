57cf9c63eea3baeed8553b2357a4c009c252c003
VILLAN3LL3 <development@gutfrau.de>
2021-05-13 22:39:00 +0200

    Add project files.

---
+++ FromBodyAndRoute/Controllers/UsersController.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace FromBodyAndRoute.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class UsersController : ControllerBase
+    {
+        [HttpPut("{Id}")]
+        public ActionResult<UserDto> Put(UserDto userDto)
+        {
+            return Ok(userDto);
+        }
+    }
+
+    public class UserDto
+    {
+        [FromRoute]
+        public int Id { get; set; }
+        [FromBody]
+        public UserBodyDto Body { get; set; }
+    }
+
+    public class UserBodyDto
+    {
+        public string Name { get; set; }
+        public string FavoriteDish { get; set; }
+    }
+}
